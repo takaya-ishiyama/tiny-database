d3b39042edf8f28625b028336cc45d97808a373c
takaya-ishiyama <takaya.ishiyama00810@gmail.com>
2026-09-22 21:38:45 +0900

    fix

--- storage/src/log.rs
+++ storage/src/log.rs
@@ -28,3 +28,28 @@ impl Log {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use tempfile::tempfile;
+
+    #[test]
+    fn appends_encoded_record_to_file() {
+        let directory = tempfile::tempdir().unwrap();
+        let path = directory.path().join("database.log");
+
+        let record = Record::new(b"name".to_vec(), b"Taro".to_vec(), false);
+
+        let expected = record.encode().unwrap();
+
+        let mut log = Log::open(&path).unwrap();
+        log.append(&record).unwrap();
+        drop(log);
+
+        let actual = fs::read(&path).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+}
